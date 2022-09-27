52468971c001da67ac05d6d2564506d63b9ab96a
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-09-27 10:22:44 +0100

    Initial commit

---
+++ Spring-basic-security/src/main/java/dev/shegs/springboot/securtiy/SpringBasicSecurityApplication.java
@@ -0,0 +1,15 @@
+package dev.shegs.springboot.securtiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringBasicSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicSecurityApplication.class, args);
+	}
+
+}
---
+++ Spring-basic-security/src/test/java/dev/shegs/springboot/securtiy/SpringBasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shegs.springboot.securtiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
