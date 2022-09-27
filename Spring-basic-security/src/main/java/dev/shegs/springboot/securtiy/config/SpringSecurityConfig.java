70a7a7555ed5223bde6e9bb726be7c31bf16df0c
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-09-27 11:41:27 +0100

    Security based on role

--- Spring-basic-security/src/main/java/dev/shegs/springboot/securtiy/config/SpringSecurityConfig.java
+++ Spring-basic-security/src/main/java/dev/shegs/springboot/securtiy/config/SpringSecurityConfig.java
@@ -14,6 +14,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("shegs").password("pass").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("shege").password("password").roles("USER");
 	}
 	
 	//Security for all API
@@ -25,11 +26,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	
 	//Security based on URL
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/rest/**").fullyAuthenticated().and().httpBasic();
-	}
+	/*
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * http.csrf().disable();
+	 * http.authorizeRequests().antMatchers("/rest/**").fullyAuthenticated().and().
+	 * httpBasic(); }
+	 */
+	
+	//Security based on ROLE
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable();
+			http.authorizeRequests().antMatchers("/rest/**").hasAnyRole("ADMIN").and().httpBasic();
+		}
 	
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
